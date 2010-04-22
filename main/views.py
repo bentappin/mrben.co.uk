4c9a9e4dfa5180b3643aeb5fc79e55f4ccb08d32
Ben Tappin <ben@mrben.co.uk>
2010-04-22 11:40:23 +0100

    Stopped using generic views.

--- main/views.py
+++ main/views.py
@@ -1,10 +1,61 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.template import RequestContext
 
-from mrben.main.models import Entry
+from mrben.main.models import Entry, Category
 
+
+
+def index(request):
+	""" Homepage view. Displays a list of blogs posts. """
+	# TODO: Pagination.
+	entry_list = Entry.objects.published().order_by('-publish')
+	return render_to_response('home.html', {
+	    'entry_list': entry_list,
+	    'project': None,
+	    'link_list': None,
+	    'show_comments': False,
+	    }, context_instance=RequestContext(request))
+
+def entry_detail(request, object_id=None, slug=None):
+	""" Displays a single blog post with comments (if enabled). """
+	try:
+		if object_id:
+			entry = Entry.objects.get(id=object_id)
+		else:
+			entry = Entry.objects.get(slug=slug)
+	except Entry.DoesNotExist:
+		entry = None
+	
+	return render_to_response('entry_detail.html', {
+	    'entry': entry,
+	    'show_comments': True,
+	    }, context_instance=RequestContext(request))
+
+def category_list(request):
+	""" Lists all categories with links to blog entry lists. """
+	category_list = Category.objects.all().order_by('title')
+	return render_to_response('category_list.html', {
+	    'category_list': category_list,
+	    }, context_instance=RequestContext(request))
 
 def category_detail(request, title):
+	""" Lists all blog posts in a particular category. """
 	entry_list = Entry.objects.published().filter(categories__title=title)
-	return render_to_response('category_detail.html', { 'entry_list': entry_list,
-																											'category_title': title })
+	return render_to_response('category_detail.html', {
+	    'entry_list': entry_list,
+	    'category_title': title,
+	    'show_comments': False,
+	    }, context_instance=RequestContext(request))
+
+def _get_featured_project():
+	""" Get a blog entry suitable for a feature box. """
+	try:
+		entry = Entry.objects.published().filter(categories__title__in=['Projects','Portfolio']).order_by('?')[0]
+		return entry
+	except Entry.DoesNotExist:
+		pass
+	except IndexError:
+		pass
+	
+	return
--- urls.py
+++ urls.py
@@ -5,82 +5,27 @@ from django.views.generic import list_detail
 from django.contrib import admin
 from django.http import HttpResponseRedirect
 
-from mrben.main.models import *
-from mrben.main.views import *
+from mrben.main.views import index, entry_detail, category_detail, category_list
 from mrben.main.feeds import EntriesFeed
 
 
 admin.autodiscover()
-
-# Setup variables for generic views
-def get_featured_project():
-	queryset = Entry.objects.published().filter(categories__title__in=['Projects','Portfolio']).order_by('?')
-	if queryset:
-		return queryset[0]
-	
-def get_links():
-	return Link.objects.all()
-
-entry_info = {
-	'queryset': Entry.objects.published(),
-	'template_name': 'entry_detail.html',
-	'template_object_name': 'entry',
-	'slug_field': 'slug',
-    'extra_context': {'show_comments': True,},
-}
-
-home_page_info = {
-	'queryset': Entry.objects.published().exclude(categories__title='Projects').exclude(categories__title='Portfolio'),
-	'template_name': 'home.html',
-	'template_object_name': 'entry',
-	'extra_context': { 	'project': get_featured_project,
-	 					'link_list': get_links,
-                         'show_comments': True,
-                         'is_home': True,}
-}
-
-projects_info = {
-	'queryset': Entry.objects.published().filter(categories__title='Projects'),
-	'template_name': 'projects.html',
-	'template_object_name': 'project',
-}
-
-portfolio_info = {
-	'queryset': Entry.objects.published().filter(categories__title='Portfolio'),
-	'template_name': 'portfolio.html',
-	'template_object_name': 'portfolio',
-}
-
-categories_info = {
-	'queryset': Category.objects.all(),
-	'template_name': 'category_list.html',
-	'template_object_name': 'category',
-}
-
-category_info = {
-	'queryset': Entry.objects.published(),
-	'template_name': 'category_detail.html',
-	'template_object_name': 'entry',
-}
-
 
 # URL patterns
 urlpatterns = patterns('',
 	(r'^admin/', include(admin.site.urls)),
 
 	# Front page
-	(r'^$', list_detail.object_list, home_page_info),
+	(r'^$', index),
 
 	# Pages
-	(r'^projects/$', list_detail.object_list, projects_info),
-	(r'^portfolio/$', list_detail.object_list, portfolio_info),
-	(r'^categories/$', list_detail.object_list, categories_info),
+	(r'^categories/$', category_list),
 	(r'^category/(?P<title>[-\w]+)/$', category_detail),
 	
 	# "Blog" pages
-	(r'^(entry|project|portfolio)/(?P<object_id>\d+)/$', list_detail.object_detail, entry_info),
-	(r'^(entry|project|portfolio)/(?P<slug>[-\w]+)/$', list_detail.object_detail, entry_info),
-	
+	(r'^(entry|project|portfolio)/(?P<object_id>\d+)/$', entry_detail),
+    (r'^(entry|project|portfolio)/(?P<slug>[-\w]+)/$', entry_detail),
+    
 	# Syndication feeds
 	(r'^feed/$', EntriesFeed()),
 	
