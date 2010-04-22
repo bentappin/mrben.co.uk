1c37e6f6cf4bc52a1153f7cd1b28ec37499dd7b1
Ben Tappin <ben@mrben.co.uk>
2010-04-22 16:25:35 +0100

    Added pagination and generic Entry list.

--- main/views.py
+++ main/views.py
@@ -1,21 +1,16 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 from mrben.main.models import Entry, Category
 
 
-
 def index(request):
 	""" Homepage view. Displays a list of blogs posts. """
-	# TODO: Pagination.
-	entry_list = Entry.objects.published().order_by('-publish')
-	return render_to_response('home.html', {
-	    'entry_list': entry_list,
-	    'project': None,
-	    'link_list': None,
-	    'show_comments': False,
-	    }, context_instance=RequestContext(request))
+	entry_list = (Entry.objects.published().exclude(categories__title='Projects')
+	                .exclude(categories__title='Portfolio').order_by('-publish'))
+	return _paginated_entry_list(request, 'Some of my news', entry_list)
 
 def entry_detail(request, object_id=None, slug=None):
 	""" Displays a single blog post with comments (if enabled). """
@@ -39,12 +34,32 @@ def category_list(request):
 	    'category_list': category_list,
 	    }, context_instance=RequestContext(request))
 
-def category_detail(request, title):
+def category_detail(request, category_title):
 	""" Lists all blog posts in a particular category. """
-	entry_list = Entry.objects.published().filter(categories__title=title)
-	return render_to_response('category_detail.html', {
-	    'entry_list': entry_list,
-	    'category_title': title,
+	entry_list = Entry.objects.published().filter(categories__title=category_title)
+	return _paginated_entry_list(request, category_title, entry_list)
+
+def _paginated_entry_list(request, title, entry_list):
+	""" Generic view for displaying paginated lists of Entry instances. """
+	paginator = Paginator(entry_list, 4)
+	
+	# See if a specific page is required, if not default to first.
+	try:
+		page = int(request.GET.get('page', '1'))
+	except ValueError:
+		page = 1
+
+	# Grab paginated set. If request page is out of range, show last page.
+	try:
+		entries = paginator.page(page)
+	except (EmptyPage, InvalidPage):
+		entries = paginator.page(paginator.num_pages)
+	
+	return render_to_response('entry_list.html', {
+	    'title': title,
+	    'entries': entries,
+	    'project': None,
+	    'link_list': None,
 	    'show_comments': False,
 	    }, context_instance=RequestContext(request))
 
@@ -53,9 +68,6 @@ def _get_featured_project():
 	try:
 		entry = Entry.objects.published().filter(categories__title__in=['Projects','Portfolio']).order_by('?')[0]
 		return entry
-	except Entry.DoesNotExist:
+	except (Entry.DoesNotExist, IndexError):
 		pass
-	except IndexError:
-		pass
-	
 	return
