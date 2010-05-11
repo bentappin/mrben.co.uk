129653350f31f2ce6ba9349fb441947328b9596f
Ben Tappin <ben@mrben.co.uk>
2010-05-11 17:01:50 +0100

    Custom Tiny MCE buttons.

--- static/js/admin/entry.js
+++ static/js/admin/entry.js
@@ -1,6 +1,10 @@
 $().ready( function () {
 	$('textarea.tinymce').tinymce({
 		script_url : '/static/js/tiny_mce/tiny_mce.js',
-		theme : 'simple',
+		theme : 'advanced',
+		theme_advanced_buttons1 : 'bold, italic, underline, strikethrough, separator,undo, redo, separator, cleanup, separator, link, unlink, image, separator, bullist, numlist, separator, outdent, indent, separator, blockquote, separator, code',
+		theme_advanced_buttons2 : '',
+		theme_advanced_buttons3 : '',
+
 	});
 });
