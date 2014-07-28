8e478c24d8a24a57148b7498245436a7cbc31500
Ben Tappin <ben@mrben.co.uk>
2014-07-28 21:48:03 +0100

    Remove group from gunicorn script.

--- server/gunicorn_mrben.sh
+++ server/gunicorn_mrben.sh
@@ -5,7 +5,6 @@ LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=4
 # user/group to run as
 USER=web-mrben
-GROUP=web
 test -d $LOGDIR || mkdir -p $LOGDIR
 exec /home/web-mrben/.virtualenvs/mrben/bin/python /home/web-mrben/.virtualenvs/mrben/bin/gunicorn_django \
 	-w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug \
