9b8bc50679db9ca03f86289c3d92bbb9f2b3bd5d
Ben Tappin <ben@mrben.co.uk>
2014-07-28 22:00:27 +0100

    Update gunicorn and rewrite runner.

--- server/gunicorn_mrben.sh
+++ server/gunicorn_mrben.sh
@@ -3,11 +3,15 @@ set -e
 LOGFILE=/var/local/sites/mrben/logs/gunicorn.log
 LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=4
-# user/group to run as
 USER=web-mrben
 GROUP=web-mrben
+ADDRESS=127.0.0.1:8000
 test -d $LOGDIR || mkdir -p $LOGDIR
-cd /var/local/sites/mrben/mrben &&
-	/home/web-mrben/.virtualenvs/mrben/bin/python /home/web-mrben/.virtualenvs/mrben/bin/gunicorn_django \
-		-w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug \
-		--log-file=$LOGFILE 2>>$LOGFILE
+cd /var/local/sites/mrben && \
+  source /home/web-mrben/.virtualenvs/mrben/bin/activate &&
+  /home/web-mrben/.virtualenvs/mrben/bin/gunicorn \
+  -b $ADDRESS -w $NUM_WORKERS \
+  --user=$USER --group=$GROUP --log-level=debug \
+  --log-file=$LOGFILE \
+  mrben.wsgi:application \
+  2>>$LOGFILE
