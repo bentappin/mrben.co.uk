1b48717759d919a156064d208de89e5554cc744c
Ben Tappin <ben@mrben.co.uk>
2014-07-28 21:52:01 +0100

    Update gunicorn exec.

--- server/gunicorn_mrben.sh
+++ server/gunicorn_mrben.sh
@@ -7,6 +7,7 @@ NUM_WORKERS=4
 USER=web-mrben
 GROUP=web-mrben
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec /home/web-mrben/.virtualenvs/mrben/bin/python /home/web-mrben/.virtualenvs/mrben/bin/gunicorn_django \
-	-w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug \
-	--log-file=$LOGFILE 2>>$LOGFILE
+cd /var/local/sites/mrben/mrben &&
+	/home/web-mrben/.virtualenvs/mrben/bin/python /home/web-mrben/.virtualenvs/mrben/bin/gunicorn_django \
+		-w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug \
+		--log-file=$LOGFILE 2>>$LOGFILE
