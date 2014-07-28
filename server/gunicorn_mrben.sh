a3299102b6225a97db1579ae98e98ca170e5b782
Ben Tappin <ben@mrben.co.uk>
2014-07-28 21:49:44 +0100

    Try with same group as user.

--- server/gunicorn_mrben.sh
+++ server/gunicorn_mrben.sh
@@ -5,6 +5,7 @@ LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=4
 # user/group to run as
 USER=web-mrben
+GROUP=web-mrben
 test -d $LOGDIR || mkdir -p $LOGDIR
 exec /home/web-mrben/.virtualenvs/mrben/bin/python /home/web-mrben/.virtualenvs/mrben/bin/gunicorn_django \
 	-w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug \
